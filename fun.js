2704dfaeac59671338d17643fbc04fde439af1eb
bkhanra <biswajitkhanra@gmail.com>
2024-11-30 22:59:52 +0530

    Updated index.html, style.css, and fun.js for layout adjustments

---
+++ fun.js
@@ -0,0 +1,22 @@
+const heading = document.querySelector("#heading");
+const questionContainer = document.querySelectorAll("#question-container");
+const body = document.querySelector("body");
+
+const colors = {
+  q1: "green",
+  q2: "red",
+  q3: "yellow",
+  q4: "cyan",
+  q5: "blue",
+  q6: "magenta",
+  q7: "orange",
+  q8: "brown",
+  q9: "purple",
+  q10: "violet",
+};
+
+for (let i = 1; i <= questionContainer.length; i++) {
+  let element = "q" + i;
+  document.getElementById(element).style.color =
+    colors[elementelement.innerHTML];
+}
